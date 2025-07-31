710961a4afb4d136590d5fe29036395541abc385
Hbitz <53080862+Hbitz@users.noreply.github.com>
2025-07-31 09:52:40 +0200

    UpdateCategoryCommand - Added validation

---
+++ AdvancedTodoApp.Application/Features/Categories/Commands/UpdateCategoryCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace AdvancedTodoApp.Application.Features.Categories.Commands
+{
+    public class UpdateCategoryCommandValidator : AbstractValidator<UpdateCategoryCommand>
+    {
+        public UpdateCategoryCommandValidator() 
+        {
+            RuleFor(x => x.CategoryId)
+                .NotEmpty().WithMessage("Category ID is required.");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Category name is required.")
+                .Must(name => !string.IsNullOrWhiteSpace(name)).WithMessage("Name cannot be whitespace")
+                .MaximumLength(100).WithMessage("Category name must be less than 100 characters");
+
+        
+        }
+    }
+}
